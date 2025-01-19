ff6c3f665e999a16c071df30323fc3ca67686df1
Satyam Singh <231220054@nitdelhi.ac.in>
2025-01-19 15:24:33 +0530

    added the required files

--- app.py
+++ app.py
@@ -1,12 +1,8 @@
 import string
 import nltk
-
+nltk.download('punkt')
 nltk.download('stopwords')
 
-nltk.data.path.append('/path/to/nltk_data')  # Specify a path to save the NLTK data
-
-# Download the punkt_tab tokenizer models
-nltk.download('punkt', download_dir='/path/to/nltk_data')
 
 import streamlit as st
 import pickle
